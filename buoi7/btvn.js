52f49726d6a7571f50ddc31ae41913511ef79b83
letoan312 <levuanhtoan@gmail.com>
2022-10-16 21:48:05 +0700

    btvn buoi 7

---
+++ buoi7/btvn.js
@@ -0,0 +1,73 @@
+/*
+    // cau truc file: ten file . js
+    cmt 1 dong: //
+    cmt nhieu dong: /* 
+*/
+
+// khai bao bien
+// var a;
+// let b;
+// const c = 123;
+
+// let x;
+// const y = 12;
+// console.log(y);
+// x = 2;
+// console.log(x);
+
+// let s = 1 + "1";
+// let t = 1 - "1";
+// console.log(s);
+// console.log(t);
+
+// if (condition) {
+
+// } else {
+
+// }
+
+// if (con) {
+
+// } else if (con) {
+
+// } else {
+    
+// }
+
+let a = 300;
+let b = 16;
+let myName = "LVAT";
+
+console.log(a);
+console.log(b);
+console.log(myName);
+
+let tong = a + b;
+let hieu = a - b;
+let tich = a * b;
+let thuong = a / b;
+
+console.log(tong);
+console.log(hieu);
+console.log(tich);
+console.log(thuong);
+
+if (a < 100) {
+    console.log("a<100");
+} else if (100 <= a < 200) {
+    console.log("100<=a<200");
+} else if (200 <= a < 300) {
+    console.log("200<=a<300");
+} else {
+    console.log("a>=300");
+}
+
+if (b < 1) {
+    console.log("b<1");
+} else if (1 <= b < 10) {
+    console.log("1<=b<10");
+} else if (10 <= b < 20) {
+    console.log("10<=b<20");
+} else {
+    console.log("a>=20");
+}
