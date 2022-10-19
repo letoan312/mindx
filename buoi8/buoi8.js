f19414b7e833af4752ee6196a03fef5bebe0f87d
letoan312 <levuanhtoan@gmail.com>
2022-10-19 22:24:21 +0700

    b8

---
+++ buoi8/buoi8.js
@@ -0,0 +1,69 @@
+// const DEFAULT_AGE = 18;
+// let myAge = prompt("Ban bao nhieu tuoi");
+
+// if (isNaN(Number(myAge))) {
+//     console.log("Ban nhap sai");
+// } else if (myAge > DEFAULT_AGE) {
+//     console.log("Ban du dk ket hon")
+// } else {
+//     console.log("Hoc het cap 3 di");
+// }
+
+// let str = myAge > DEFAULT_AGE ? "Marry" : "Study";
+
+// console.log(str);
+
+
+
+// let month = 10;
+
+// switch (month) {
+//     case 1: {
+//         console.log("January");
+//         break;
+//     }
+//     case 2: {
+//         console.log("Febuary");
+//         break;
+//     }
+//     default:
+//         console.log("Ban nhap sai");
+//         break;
+// }
+
+// for (let i = 0; i < 101; i++) {
+//     console.log(i);
+// }
+
+// for (let x = 1; x < 10; x++) {
+//     if (x % 2 === 0) {
+//         console.log("So chan: ", x);
+//     }
+// }
+
+// let result = 0;
+// let n = 15;
+// for (let a = 1; a < n; a++) {
+//     if (a % 7 === 0) {
+//         result = a;
+//         console.log(a);
+//     }
+// }
+
+// console.log("Done: ", result);
+
+
+// for (let x = 1; x < 10; x++) {
+//     for (let y = 1; y < 10; y++) {
+//         console.log(`${x} * ${y} = ${x * y}`);
+//     }
+// }
+
+// for (let x = 1; x < 10; x++) {
+//     for (let y = 1; y < 10; y++) {
+//         if (y === 4) {
+//             continue
+//         }
+//         console.log(`${x} * ${y} = ${x*y}`);
+//     }
+// }
